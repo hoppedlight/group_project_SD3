7c4eedbc617c4a13d75ca5ff895985a724517dfc
hoppedlight <61391150+hoppedlight@users.noreply.github.com>
2025-01-25 17:35:40 +0100

    update_react
    update_react

--- groupProject/groupApp/urls.py
+++ groupProject/groupApp/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-url_patterns = [
-  path("", views.home, name = "home")
-]
+# # url_patterns = [
+# #   path("", views.home, name = "home")
+# # ]
